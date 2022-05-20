f1abc59f4ac9e0a84026e1b70197d1568ec111fe
DRMakhutov <DRMakhutov@beeline.kz>
2022-05-20 14:38:27 +0600

    Updated version of ChatUI

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "ChatUI",
     defaultLocalization: "ru",
-    platforms: [.iOS(.v10), .macOS(.v10_12)],
+    platforms: [.iOS(.v13)],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
@@ -15,7 +15,7 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/ainurb/MessageKit.git", Package.Dependency.Requirement.branch("ios_10")),
+        .package(url: "https://github.com/MessageKit/MessageKit", Package.Dependency.Requirement.exact("3.8.0")),
         .package(url: "https://github.com/SnapKit/SnapKit.git", .upToNextMajor(from: "5.0.1"))
     ],
     targets: [
